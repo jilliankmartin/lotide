89713f71721533ab9e099d22babefd9edd780f5c
jilliankmartin <jillmartin@live.com>
2020-09-15 22:39:09 +0000

    Added a function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+// receive two arrays 
+// check if the two arrays match one another
+//return either true or false
+
+const eqArrays = function(array1, array2) {
+  let arrayStringOne = array1.toString();
+  let arrayStringTwo = array2.toString();
+  if (arrayStringOne === arrayStringTwo) {
+    return true
+  }
+
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
