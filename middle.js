4e686b7abd5d0f8a9437c46859c21b7486a6efa8
jilliankmartin <jillmartin@live.com>
2020-09-21 15:44:39 +0000

    removed tests and added to test file

--- middle.js
+++ middle.js
@@ -1,22 +1,4 @@
-//TESTER FUNCTIONS
 
-const eqArrays = function(array1, array2) {
-  let arrayStringOne = array1.toString();
-  let arrayStringTwo = array2.toString();
-  if (arrayStringOne === arrayStringTwo) {
-    return true;
-  }
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
-//SOLUTION
 
 //determine whether the array has an odd number of elements or an even number
 const isItEven = function(myArray) {
@@ -27,8 +9,6 @@ const isItEven = function(myArray) {
   }
 };
 
-//receive some input (array)
-//return the middle array element
 
 const findMiddle = function(myArray) {
   let arrMiddle = [];
@@ -44,7 +24,7 @@ const findMiddle = function(myArray) {
   return arrMiddle;
 };
 
-assertArraysEqual([findMiddle([1,2,3])], [2]);
+module.exports = findMiddle;
 
 
 
